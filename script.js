14183c1806e10ae4652b9d5274ba918a474112b7
DavrosB <dave_bell@blueyonder.co.uk>
2021-07-13 10:19:48 +0100

    db added script file

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Davy");
+
+console.info("Bell");
+
+console.warn("Scotland");
+
+console.error("Pisces");
+
+
+let myCar = "Honda";
+let myModel = "CRV";
+console.log("My actual car is: " + myCar + " and the best model is: " + myModel);
+
+
+
+let myCar1 = "Honda";
+let myModel1 = "CRV";
+console.log(`my fav car is ${myCar1} and the model is ${myModel1}`);
+
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+console.log (typeof(a));
+console.log (typeof(b));
+console.log (typeof(c));
+console.log (typeof(d));
+console.log (typeof(e));
+
+
