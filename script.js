81bc0c571008e558a899cb8e5df6937910ae237d
DavrosB <dave_bell@blueyonder.co.uk>
2021-07-13 12:13:33 +0100

    db added Exercise 1 using for loop

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-console.log("Davy");
+//console.log("Davy");
 
-console.info("Bell");
+//console.info("Bell");
 
-console.warn("Scotland");
+//console.warn("Scotland");
 
-console.error("Pisces");
-console.log("BigBalls");
+//console.error("Pisces");
+//console.log("BigBalls");
 
-let myCar = "Honda";
-let myModel = "CRV";
-console.log("My actual car is: " + myCar + " and the best model is: " + myModel);
+//let myCar = "Honda";
+//let myModel = "CRV";
+//console.log("My actual car is: " + myCar + " and the best model is: " + myModel);
 
 
 
-let myCar1 = "Honda";
-let myModel1 = "CRV";
-console.log(`my fav car is ${myCar1} and the model is ${myModel1}`);
+//let myCar1 = "Honda";
+//let myModel1 = "CRV";
+//console.log(`my fav car is ${myCar1} and the model is ${myModel1}`);
 
 
 //let a;
-let b = "12345";
-let c = 12344;
-let d = true;
-let e = {a:"JavaScript"};
+//let b = "12345";
+//let c = 12344;
+//let d = true;
+//let e = {a:"JavaScript"};
 
 //console.log (typeof(a));
-console.log (typeof(b));
-console.log (typeof(c));
-console.log (typeof(d));
-console.log (typeof(e));
+//console.log (typeof(b));
+//console.log (typeof(c));
+//console.log (typeof(d));
+//console.log (typeof(e));
 
 
-let totalMoney = 4000;
-let moneyPaidSoFar = 2348;
-let totalLeftToPay = totalMoney - moneyPaidSoFar;
+//let totalMoney = 4000;
+//let moneyPaidSoFar = 2348;
+//let totalLeftToPay = totalMoney - moneyPaidSoFar;
 
-console.log(`The customer's bill is £${totalMoney} the remaining amount of money left to be paid is £${totalLeftToPay}`);
+//console.log(`The customer's bill is £${totalMoney} the remaining amount of money left to be paid is £${totalLeftToPay}`);
 
-console.log(`The customers bill so far is £${totalMoney} the amount left to pay is £${totalMoney - totalLeftToPay}`);
+//console.log(`The customers bill so far is £${totalMoney} the amount left to pay is £${totalMoney - totalLeftToPay}`);
 
 
 //Exercise 4
@@ -62,16 +62,21 @@ console.log(`The customers bill so far is £${totalMoney} the amount left to pay
 //}
 
 //Exercise 6
-let number = 1;
-while (number <= 10) {
+//let number = 1;
+//while (number <= 10) {
 
-for (let i =1; i<=10; i++) { 
-    console.log(number);
-}
-let i=1;
-number++;
+//for (let i =1; i<=10; i++) { 
+ //   console.log(number);
+//}
+//let i=1;
+//number++;
+//
+//}
 
-}
+//Exercise 7
+for (let a = 100; a <= 200; a++) {
+    console.log(`a = ${a}`);
+  }
 
-
+  
 
