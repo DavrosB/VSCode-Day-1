8917758d712182ba3201faa41a73973508cbe4bc
DavrosB <dave_bell@blueyonder.co.uk>
2021-07-14 10:49:51 +0100

    db added Function examples

---
+++ function.js
@@ -0,0 +1,34 @@
+`use strict`;
+
+// Function example 1
+// Create a function that takes in 2 parameters num1 and num2 
+// and subtracts the two numbers.
+
+//const subtraction = (a,b) => console.log(a-b);
+// subtraction(28,3);
+
+// Function Example 2
+//Create a function expression called welcome that take in name,
+//  age,gender as a parameters. The outcome should be like so:
+//e.g. "My name is Felix Cited, i am 20 years old and of gender Male"
+
+// const welcome = (name, age, gender) => console.log(`My name is ${name}, I am ${age} years old and of gender ${gender}`);
+
+// welcome(`Davy`, 51, `male`);
+
+// Function example 3
+//Create an arrow function called powerUp that takes in two values
+// n1 and n2. The arrow function will return the power of the two numbers.
+
+// const powerUp = (n1, n2) => {
+    // let n1=3;
+    // let n2=3;
+    // powerUp = (n1,n2) => Math.pow(n1,n2);
+    // powerNum = powerUp(3,3);
+    // console.log(`The number ${n1} to the power of ${n2} equals ${powerNum}`)
+
+// Alternative
+// powerUp = (n1,n2) => Math.pow(n1,n2);
+// let n1=3;
+// let n2=3;
+// console.log(`Number ${n1} to the power ${n2} is ${powerUp(3,3)}`);
